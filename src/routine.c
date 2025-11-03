4648683f2af38165df4d6decc8b357c630ceb073
Waltteri Heino <waltteriheino@gmail.com>
2025-11-03 13:49:45 +0200

    Modified: Even philo delay happens on all cases, previously delayed only when n_philos was odd. Modified: Take_forks() - simplified fork pick up order to 1 then 2 for all philos. These modifications fixed philos dying in the no-death test.

--- src/routine.c
+++ src/routine.c
@@ -14,7 +14,7 @@ void	*philo_routine(void *philo_data)
 	while (get_end_sim(philo->table) == FALSE
 		&& get_meals_eaten(philo) < philo->table->n_meals)
 	{
-		if ((philo->table->n_philos % 2) == 1 && (philo->id % 2) == 0)
+		if ((philo->id % 2) == 0)
 			usleep((philo->table->t_eat / 2) * 1000);
 		think(philo);
 		take_forks(philo);
@@ -50,29 +50,16 @@ void	think(t_philo *philo)
 }
 
 // Take fork == Attempt to lock 'fork' mutex.
-// Pick up order:
-// 	- Even philos - fork1 (left), then fork2 (right).
-// 	- Odd philos  - fork2 (right), then fork1 (left).
+// Once mutex locked - Prints "[timestamp] [philo_id] has taken a fork".
 void	take_forks(t_philo *philo)
 {
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->fork1);
-		printf("%ld %d has taken a fork\n",
-			time_since_start_ms(philo->table->start_ms), philo->id);
-		pthread_mutex_lock(philo->fork2);
-		printf("%ld %d has taken a fork\n",
-			time_since_start_ms(philo->table->start_ms), philo->id);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->fork2);
-		printf("%ld %d has taken a fork\n",
-			time_since_start_ms(philo->table->start_ms), philo->id);
-		pthread_mutex_lock(philo->fork1);
-		printf("%ld %d has taken a fork\n",
-			time_since_start_ms(philo->table->start_ms), philo->id);
-	}
+
+	pthread_mutex_lock(philo->fork1);
+	printf("%ld %d has taken a fork\n",
+		time_since_start_ms(philo->table->start_ms), philo->id);
+	pthread_mutex_lock(philo->fork2);
+	printf("%ld %d has taken a fork\n",
+		time_since_start_ms(philo->table->start_ms), philo->id);
 }
 
 // Sets last_meal to current timestamp when philo begins eating.
